19a4d392839d2572a4382d802269709e304be7ba
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-12-07 15:45:22 +0300

    maybe

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="menu">
       <ul>
-        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/" exact>Home</router-link></li>
         <li><router-link to="/list-books">List Books</router-link></li>
         <li><router-link to="/feedback">Feedback</router-link></li>
       </ul>
@@ -9,18 +9,49 @@
   </template>
   
   <style scoped>
+  .menu {
+    text-align: center;
+  }
+  
+  .menu ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: inline-flex;
+  }
+  
+  .menu li {
+    margin: 0 10px;
+    border: 1px solid #ccc; /* Border for menu items */
+    border-radius: 5px; /* Rounded corners */
+    transition: background-color 0.3s; /* Smooth transition for hover effect */
+  }
+  
+  .menu a {
+    display: block;
+    padding: 10px 15px;
+    text-decoration: none;
+    color: inherit; /* Inherits from .menu class */
+  }
+  
+  .menu a.router-link-exact-active, /* Styling for active menu item */
+  .menu a.router-link-active { /* Vue Router adds these classes automatically */
+    background-color: gray;
+    color: black;
+  }
+  
+  .menu a:hover { /* Hover effect */
+    background-color: lightgray;
+    color: black;
+  }
+  
+  @media (max-width: 600px) { /* Mobile responsiveness */
     .menu ul {
-      list-style-type: none;
-      padding: 0;
-      margin: 0;
-      /* Add other styles as needed */
+      flex-direction: column;
     }
     .menu li {
-      display: inline-block;
-      margin: 0 10px;
+      margin: 5px;
     }
-    .menu a {
-      color: inherit; /* Inherits from .menu class */
-    }
+  }
   </style>
   
